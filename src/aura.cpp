3f9a91b0ed7cf57a16e4e772ccaa79606d7cd757
Adiel Julio <adieljulio@gmail.com>
2023-11-10 00:36:12 +0700

    add stack trace

--- src/aura.cpp
+++ src/aura.cpp
@@ -35,6 +35,11 @@
 #include "util.h"
 #include "fileutil.h"
 
+#include <stdio.h>
+#include <execinfo.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <csignal>
 #include <cstdlib>
 #include <thread>
@@ -68,12 +73,28 @@ void Print2(const string& message)
     gAura->m_IRC->SendMessageIRC(message, string());
 }
 
+void crashHandler(int sig) {
+  void *array[10];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 10);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
 //
 // main
 //
 
 int main(const int, const char* argv[])
 {
+  // add crash handler
+  signal(SIGSEGV, crashHandler);
+
   // seed the PRNG
 
   srand(static_cast<uint32_t>(time(nullptr)));
